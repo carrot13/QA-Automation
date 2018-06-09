db2284f1375a7f14a5d5e545d9c8324297611e21
Mitko Stamatov <mitco_s@hotmail.com>
2018-06-09 21:30:12 +0300

    added wait element

--- SoftUniLogInAutomattionTest/Program.cs
+++ SoftUniLogInAutomattionTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
 
 namespace SoftUniLogInAutomattionTest
 {
@@ -10,17 +12,33 @@ namespace SoftUniLogInAutomattionTest
         {
             string Username = "mitko.stamatov";
             string Password = "P10vdiv1319";
+
+            //Chrome Options
             ChromeOptions options = new ChromeOptions();
             options.AddArguments("disable-infobars");
             options.AddArguments("--start-maximized");
 
             IWebDriver driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://softuni.bg");
-            driver.Navigate().Refresh();
+
             driver.FindElement(By.LinkText("Вход")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+
+            IWebElement loginButton = wait.Until<IWebElement>
+                ((w) => { return w.FindElement(By.CssSelector(".btn.btn-primary.btn-lg.btn-long.top-buffer.col-md-12")); });
+
             driver.FindElement(By.Id("username")).SendKeys(Username);
             driver.FindElement(By.Id("password")).SendKeys(Password);
-            driver.FindElement(By.CssSelector(".btn.btn-primary.btn-lg.btn-long.top-buffer.col-md-12")).Click();
+
+            loginButton.Click();
+
+            //wait
+            Thread.Sleep(5000);
+
+            //close
+            driver.Close();
+
         }
     }
 }
